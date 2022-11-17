5f9de3ebf11caab1b4c72aea86e2de0f0054d891
maimuna99 <73959703+maimuna99@users.noreply.github.com>
2022-11-17 19:58:11 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-fav-movies-t1vqrt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/my-fav-movies-t1vqrt)
---
+++ src/App.vue
@@ -0,0 +1,109 @@
+<script setup>
+/*
+I tried to create a handy webapp to keep track of all the movies I've watched this year and which was my favorite, but form bindings are confusing and it's not working properly :(
+
+Can you help me fix it?
+
+level 1: Update the movie list with a v-for loop so all movies in the provided file movies.json are rendered. Feel free to update/change the list to your likes, or to add more info for each movie.
+
+level 2: Create a small "movie card" for each movie object in the list and display in it the title, a small poster picture, and the score of the movie.
+
+level 3: Remove the duplicated & hardcoded code in the form binding for "My fav movie" selector 1, and use a v-for loop and attribute bindings to correctly build the options. (Note: you can change the binding variable 'favoriteMovie' below if needed)
+
+level 4: I want it to be easy to see which one is my current favorite, so please add a "😍" emoji next to the title to my favorite movie in the list.
+
+level 5: The score of each film is represented by a string with a value between "0" and "100". We can do better! Let's show between 1 to 5 stars in each film card, according to the score (tip: use a v-for loop here).
+
+level 6: Fix the multiselectors 2, 3 of the movies I've watched this year. Remove the duplicated & hardcoded code in the form binding for both multi selectors, and use v-for loops and attribute bindings to correctly build the options. Keep in mind they should be synchronized! (keep using the same binding to the variable "watchedList" for both fields).
+
+level 7: The multiselectors are nice, but it would be great to also have a checkbox on each film card to mark it as watched/not watched. Of course, it should be synchronized with the "watchedList" array!
+
+bonus level: Apply your CSS magic and designer's touch to make the app look great!
+*/
+
+import { ref } from 'vue';
+import movies from './assets/movies.json';
+
+const favoriteMovie = ref('');
+const watchedList = ref([]);
+</script>
+
+<template>
+  <h3>Movie list</h3>
+  <div>
+    <ul  v-for="movie in movies">
+
+    <div class="card">
+      <img src="{{movie.picture}}" alt="Avatar" style="width:100%">
+      <div class="container">
+      <h4><b>Movie Title : {{movie.title}}</b></h4> 
+      <p>Movie Score :  {{movie.score}}</p> 
+  </div>
+</div>
+
+      <li>
+         {{movie}} 
+      </li>
+    </ul>
+  </div>
+
+  <hr />
+  <h3>Controls</h3>
+  <div>
+    <span>1 - My fav movie: {{ favoriteMovie || 'none chosen yet :(' }}</span>
+    <br />
+    <br />
+    <select v-model="favoriteMovie">
+      <option disabled value="">Please select a favorite!</option>
+      <option v-for="movie in movies">
+        {{ movie.title }}
+      </option>
+    </select>
+  </div>
+
+  <hr />
+
+  <div>
+    <span>2 - Movies watched this year: {{ watchedList }}</span>
+    <br />
+    <br />
+    <select v-model="watchedList" multiple>
+      <option>Dune</option>
+      <option>Everything, Everywhere, all at once</option>
+      <option>Top Gun: Maverick</option>
+    </select>
+  </div>
+
+  <hr />
+  <div>
+    <span>3 - Movies watched this year: {{ watchedList }}</span>
+    <br />
+    <br />
+
+    <input type="checkbox" id="option1" value="Dune" v-model="watchedList" />
+    <label for="option1">Dune</label>
+    <br />
+
+    <input type="checkbox" id="option2" value="?" v-model="watchedList" />
+    <label for="option2">Everything, Everywhere, all at once</label>
+    <br />
+    <input type="checkbox" id="option3" value="?" v-model="watchedList" />
+    <label for="option3">Top Gun: Maverick</label>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 40%;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+</style>
